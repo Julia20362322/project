693c5916544659bb347bc1601e9af04eb8f2958e
Julia Cowper <20362322@student.uwa.edu.au>
2013-05-21 13:28:17 +0800

    Revert "project"
    This reverts commit 95456a495bf5515e5e05617dc59a264a6795ca58.

--- todo-master/app/models/item.rb
+++
@@ -1,4 +0,0 @@
-class Item < ActiveRecord::Base
-  belongs_to :list
-  attr_accessible :completed, :content, :position
-end
--- todo-master/app/models/list.rb
+++
@@ -1,5 +0,0 @@
-class List < ActiveRecord::Base
-  has_many :accesses
-  has_many :users, through :accesses
-  attr_accessible :name
-end
